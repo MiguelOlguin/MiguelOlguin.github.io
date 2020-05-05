df30934099b1aa19f692a44d3e87b3aa6376d6e9
MiguelOlguin <45398531+MiguelOlguin@users.noreply.github.com>
2020-05-04 22:59:41 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 /*!
-    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
+ * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
+ */
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -21,7 +21,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -32,7 +32,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
     } else {
@@ -58,5 +58,63 @@
     image: {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
     }
-  });
+  });/*
+  $(document).ready(function () {
+
+  }
+
+  function getCategorias() {
+    let dropdown = document.getElementById('categorias');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement("option");
+    defaultOption.text = 'Elegí una categoria';
+
+    dropdown.appendChild(defaultOption);
+    dropdown.options[dropdown.options.length] = new Option('Text 1', 'Value1');
+    const url = 'http://192.168.1.43:4004/categorias';
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function () {
+      if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        let option;
+
+        for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+          option.text = data[i].nombre;
+          dropdown.options[dropdown.options.length] = new Option(option.text, i);
+        }
+      } else {
+        // Reached the server, but it returned an error
+      }
+    }
+
+    request.onerror = function () {
+      console.error('An error occurred fetching the JSON from ' + url);
+    };
+
+    request.send();
+  }
+  /*
+  function getCategorias(listName, siteurl, success, failure) {
+    $.ajax({
+      url: "localhost:4004/categorias",
+      method: "GET",
+      headers: {
+        "Accept": "application/json; odata=verbose"
+      },
+      success: function (data) {
+        console.log(data);
+        success(data);
+      },
+      error: function (data) {
+        failure(data);
+        console.log("Falla");
+      }
+    });
+  }
+  */
+ 
 })(jQuery); // End of use strict
